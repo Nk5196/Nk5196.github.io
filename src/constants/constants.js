1e7f72689d045518a8fa93809e915a3e1657c6fa
Nk5196 <narendrakandalkar@gmail.com>
2023-09-02 11:45:41 +0530

    updated projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,22 +1,24 @@
 export const projects = [
+  {
+    title: 'Dnyanodaya School',
+    description: "This is a school project which has public portal and on login it has admin panel to take attendance and list students. login details - Id - dny, pass- 123",
+      image: '/images/dny.png',
+      tags: [ 'React.js','Node.js','Express.js','MongoDB','Chakra UI','JavaScript', 'CSS3', 'HTML'],
+    source: 'https://github.com/Nk5196',
+    visit: 'https://dnyanodaya-frontend.vercel.app',
+    id: 0,
+  },
     {
     title: 'YouTube.com',
     description: "YouTube is an American online video-sharing and social media platform headquartered in San Bruno, California, United States. Accessible worldwide, it was launched on February 14, 2005, by Steve Chen, Chad Hurley, and Jawed Karim.",
-      image: '/images/3.png',
-      tags: ['React', 'Redux', 'Tailwind','JavaScript],
+      image: '/images/youtube.png',
+      tags: ['React', 'Redux', 'Tailwind','JavaScript'],
     source: 'https://github.com/Nk5196/youtube',
     visit: 'https://youtube-kappa-coral.vercel.app/',
-    id: 0,
-  },
-  {
-    title: 'Shopbop.com',
-    description: "Shopbop is the global online retail destination for style inspiration and discovery, offering women and men a curated selection of ready-to-wear & accessories from over 1,000 established and emerging designers.",
-      image: '/images/1.png',
-      tags: [ 'JavaScript', 'Css3', 'HTML'],
-    source: 'https://github.com/sarb908/ShopBop',
-    visit: 'https://shopbop009.netlify.app/',
     id: 1,
   },
+
+ 
   {
     title: 'Reliance-Digital',
     description:"Reliance Digital is an Indian consumer electronics retailer. It is a subsidiary of Reliance Retail, a wholly owned subsidiary of Reliance Industries. Reliance Digital opened its first store on 24 April 2007 in Delhi.",
@@ -34,6 +36,15 @@ export const projects = [
     source: 'https://github.com/Nk5196/dizzy-stove-3148',
     visit: 'https://nk5expedia.netlify.app/',
     id: 3,
+  },
+  {
+    title: 'Shopbop.com',
+    description: "Shopbop is the global online retail destination for style inspiration and discovery, offering women and men a curated selection of ready-to-wear & accessories from over 1,000 established and emerging designers.",
+      image: '/images/1.png',
+      tags: [ 'JavaScript', 'Css3', 'HTML'],
+    source: 'https://github.com/sarb908/ShopBop',
+    visit: 'https://shopbop009.netlify.app/',
+    id: 4,
   },
 
 ];
